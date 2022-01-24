b52457a9b19f6aebbb72ed0c2eb4ab990118fe35
Shawn Parker <shawn@topfroggraphics.com>
2022-01-24 13:55:23 -0800

    Fix SVG size for text-only browsing

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -15,6 +15,8 @@ const icon = icons[name]
 
 <svg
   role="img"
+  width="16"
+  height="16"
   viewBox="0 0 {icon.viewBox.w} {icon.viewBox.h}"
   xmlns="http://www.w3.org/2000/svg"
 ><title>{@html icon.title}</title><g>{@html icon.svg}</g></svg>
