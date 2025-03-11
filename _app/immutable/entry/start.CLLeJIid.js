477b0c7f0d2bc6b88b97d3e61f65c30fe0f26f5e
Gipetto <Gipetto@users.noreply.github.com>
2025-03-11 22:44:30 +0000

    Deploying to gh-pages from @ Gipetto/resume@32ddb5bb79a3b228860867a843d827999a8dd819 🚀

--- _app/immutable/entry/start.CLLeJIid.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/DgX2VLoc.js";export{t as start};
-//# sourceMappingURL=start.CLLeJIid.js.map
---
+++ _app/immutable/entry/start.DS6wrhMA.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/1C3QhdU3.js";export{t as start};
+//# sourceMappingURL=start.DS6wrhMA.js.map
