248714878065a42ee1ec12536e4a8883f6e5fda0
Gipetto <Gipetto@users.noreply.github.com>
2025-06-23 23:27:12 +0000

    Deploying to gh-pages from @ Gipetto/resume@77a0c3b96700ce94a3402843dcbb565223ec50ab 🚀

--- _app/immutable/entry/start.BSsF9XTl.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/DspleQcw.js";export{t as start};
-//# sourceMappingURL=start.BSsF9XTl.js.map
---
+++ _app/immutable/entry/start.BdfU-Q1f.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/D3ff1m66.js";export{t as start};
+//# sourceMappingURL=start.BdfU-Q1f.js.map
