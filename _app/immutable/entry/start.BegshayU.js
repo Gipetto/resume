3a33073ea9ad17397eb39caab7fc9373a927a4f4
Gipetto <Gipetto@users.noreply.github.com>
2025-08-13 01:58:37 +0000

    Deploying to gh-pages from @ Gipetto/resume@f6eac08d93aa030de2c09a7abda9d31bd6af9a9f 🚀

--- _app/immutable/entry/start.BegshayU.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/CuvDtl3k.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.BegshayU.js.map
---
+++ _app/immutable/entry/start.CGj_YT3R.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/BV7OIWQa.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.CGj_YT3R.js.map
