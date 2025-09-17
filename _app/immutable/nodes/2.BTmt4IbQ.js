69e86398a056767ee7728cbd4b7d381552198297
Gipetto <Gipetto@users.noreply.github.com>
2025-09-17 15:15:13 +0000

    Deploying to gh-pages from @ Gipetto/resume@862ab6ff8a4d5a49f778548c470c0a5517355b69 🚀

--- _app/immutable/nodes/2.BTmt4IbQ.js
+++
@@ -1,2 +0,0 @@
-import"../chunks/DsnmJJEf.js";import{f as i,e as n,b as e,d as t,n as d,r as p}from"../chunks/Af-DUVey.js";var l=i('<div class="grid-single"><!></div>');function c(a,r){var o=l(),s=t(o);n(s,()=>r.children??d),p(o),e(a,o)}export{c as component};
-//# sourceMappingURL=2.BTmt4IbQ.js.map
