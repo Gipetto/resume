58c71e64b73f1669e23389c9ac5b62f7d6b874ec
Gipetto <Gipetto@users.noreply.github.com>
2025-12-14 20:22:26 +0000

    Deploying to gh-pages from @ Gipetto/resume@2d072124f9b0762ea7b39402312658842b30d082 🚀

---
+++ _app/immutable/entry/start.CaHPEPDO.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/BZiQB4RX.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.CaHPEPDO.js.map
--- _app/immutable/entry/start.HLLp-wBA.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/DmrhEZiJ.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.HLLp-wBA.js.map
